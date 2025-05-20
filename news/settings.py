6cbd055a3204f09d4832b8ee3c79d89f1226ef62
panix0612 <41118121@gcloud.csu.edu.tw>
2025-05-20 16:01:35 +0800

    settings.py

--- news/settings.py
+++ news/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-08n2ih5w3wf82f7!4u-w*$z6d4q!y^6#z!3n65on2&9mfeku%i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
